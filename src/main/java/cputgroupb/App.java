91eee7f48a85a88ad38c22d580478ef95ed2d17c
Kelvin <vansittert5@gmail.com>
2019-02-12 11:07:57 +0200

    Second attempt at intit

---
+++ src/main/java/cputgroupb/App.java
@@ -0,0 +1,17 @@
+package cputgroupb;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        JOptionPane.showInputDialog("Enter anything");
+        JOptionPane.showMessageDialog(null,"Good job!!");
+    }
+}
